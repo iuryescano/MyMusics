59e58285f8f359477a2f383bf425b4fb0cf9b457
iuryescano <iurycordeiro15@gmail.com>
2024-08-07 16:53:29 -0300

    Adding styled-components in project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Details } from './pages/Details'
+import { Details } from './pages/Details/index'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
