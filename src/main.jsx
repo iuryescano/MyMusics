e51f6ebf4ad5529bf061cd0596f9fab678a61a45
Iury Escano <yan_cordeiro@live.com>
2024-08-19 15:35:34 -0300

    [Fix] SignIn Page Created in project

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global';
 
-import { Details } from './pages/Details/index'
+import { SignIn } from './pages/SignIn/index'
 
 import theme from './styles/theme'
 
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,37 @@
+import { Container, Form, Background } from "./styles";
+import { FiMail, FiLock } from 'react-icons/fi'
+import { Input } from "../../components/Input/index";
+import { Button } from "../../components/Button/index";
+
+
+export function SignIn() {
+  return(
+    <Container>
+      <Form>
+        <h1>MyMusics</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title={"Entrar"}/>
+
+        <a href="#">
+          Criar Conta
+        </a>
+      </Form>
+
+      <Background/>
+    </Container>
+  );
+}
